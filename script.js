c23a63beff5f4c45a0f25a27d8acc3c6fa9a964a
jeffch19 <17jcummings@gmail.com>
2023-10-31 13:32:01 -0400

    cleaning up code trying to fix display issues for 5 day forecast

--- script.js
+++ script.js
@@ -1,160 +1,140 @@
 $(document).ready(function() {
 
+  // API variables
+  const cityName = document.getElementById('city-name');
+  const temp = document.getElementById('temp');
+  const wind = document.getElementById('wind');
+  const humidity = document.getElementById('humidity');
+  const searchBtn = document.getElementById('search-btn');
+  const apiKey = '3b8d60dee3567443738880c9e96f75d9';
 
-// API variables
-const cityName = document.getElementById('city-name');
-const temp = document.getElementById('temp');
-const wind = document.getElementById('wind');
-const humidity = document.getElementById('humidity');
-const currentDate = new Date();
+  // Get the current date
+  const currentDate = new Date();
+  const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
+  const day = currentDate.getDate().toString().padStart(2, '0');
+  const year = currentDate.getFullYear();
+  const formattedDate = `${month}/${day}/${year}`;
 
-const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
-const day = currentDate.getDate().toString().padStart(2, '0');
-const year = currentDate.getFullYear();
+  // Set up local storage
+  var recentlySearchedCities = [];
 
-const formattedDate = `${month}/${day}/${year}`;
-var apiKey = '3b8d60dee3567443738880c9e96f75d9'
-var searchBtn = document.getElementById('search-btn');
-// var cities = ['Atlanta','Denver', 'Seattle', 'San Francisco','Orlando','New York', 'Chicago','Austin'];
-//set up local storage
-var recentlySearchedCities = [];
-
-if (localStorage.getItem('recentCities')) {
-  recentlySearchedCities = JSON.parse(localStorage.getItem('recentCities'));
-  renderRecentlySearchedCities();
-}
-
-// Function to add a city to the recently searched list
-function addToRecentlySearched(city) {
-  if (!recentlySearchedCities.includes(city)) {
-    recentlySearchedCities.push(city);
-    localStorage.setItem('recentCities', JSON.stringify(recentlySearchedCities));
+  if (localStorage.getItem('recentCities')) {
+    recentlySearchedCities = JSON.parse(localStorage.getItem('recentCities'));
     renderRecentlySearchedCities();
   }
-}
 
-// Function to render recently searched cities as buttons
-function renderRecentlySearchedCities() {
-  var bottomDiv = document.querySelector('.bottom-div');
-  bottomDiv.innerHTML = '';
+  // Function to add a city to the recently searched list
+  function addToRecentlySearched(city) {
+    if (!recentlySearchedCities.includes(city)) {
+      recentlySearchedCities.push(city);
+      localStorage.setItem('recentCities', JSON.stringify(recentlySearchedCities));
+      renderRecentlySearchedCities();
+    }
+  }
 
-  recentlySearchedCities.forEach(function(city) {
-    var cityButton = document.createElement('button');
-    cityButton.textContent = city;
-    cityButton.className = 'recent-city-button btn-spacing';
+  // Function to render recently searched cities as buttons
+  function renderRecentlySearchedCities() {
+    var bottomDiv = document.querySelector('.bottom-div');
+    bottomDiv.innerHTML = '';
 
-    cityButton.addEventListener('click', function() {
-      // When a recently searched city button is clicked, search for it again
-      getWeatherData(city);
-      getFiveDayForecast(city);
+    recentlySearchedCities.forEach(function(city) {
+      var cityButton = document.createElement('button');
+      cityButton.textContent = city;
+      cityButton.className = 'recent-city-button btn-spacing';
+
+      cityButton.addEventListener('click', function() {
+        // When a recently searched city button is clicked, search for it again
+        getWeatherData(city);
+        getFiveDayForecast(city);
+      });
+
+      bottomDiv.appendChild(cityButton);
     });
+  }
 
-    bottomDiv.appendChild(cityButton);
+  // geo code API
+  // http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+
+  // 5-day forecast API
+  // api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+
+  // Initial API call
+  function getWeatherData(city) {
+    var requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}&units=imperial`;
+
+    fetch(requestUrl)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        console.log('fetch response \n------');
+        console.log(data);
+        cityName.textContent = `${data.name} - ${formattedDate}`;
+        temp.textContent = `Temp: ${data.main.temp} °F`;
+        wind.textContent = `Wind: ${data.wind.speed} MPH`;
+        humidity.textContent = `Humidity: ${data.main.humidity} %`;
+      });
+  }
+
+  // Make 5-day forecast function
+  function getFiveDayForecast(city) {
+    var forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=${apiKey}&units=imperial`;
+
+    fetch(forecastUrl)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        console.log('5-day forecast response \n------');
+        console.log(data);
+
+        // Clear the existing forecast boxes
+        var forecastDiv = document.querySelector('.forecast-div');
+        forecastDiv.innerHTML = '';
+
+        // Loop through the forecast data for the next 5 days
+        for (var i = 0; i < data.list.length; i += 8) {
+          var forecastData = data.list[i];
+          var date = new Date(forecastData.dt * 1000); // Convert the timestamp to a Date object
+          var formattedDate = date.toDateString();
+
+          // Create a new div element for the forecast box
+          var forecastBox = document.createElement('div');
+          forecastBox.className = 'forecast-box';
+
+          // Create separate elements for date, temperature, wind, and humidity
+          var dateElement = document.createElement('p');
+          dateElement.textContent = ` ${formattedDate}`;
+          var tempElement = document.createElement('p');
+          tempElement.textContent = `Temp: ${forecastData.main.temp} °F`;
+          var windElement = document.createElement('p');
+          windElement.textContent = `Wind: ${forecastData.wind.speed} MPH`;
+          var humidityElement = document.createElement('p');
+          humidityElement.textContent = `Humidity: ${forecastData.main.humidity} %`;
+
+          // Append these elements to the forecastBox
+          forecastBox.appendChild(dateElement);
+          forecastBox.appendChild(tempElement);
+          forecastBox.appendChild(windElement);
+          forecastBox.appendChild(humidityElement);
+
+          // Append the forecastBox to the .forecast-div
+          forecastDiv.appendChild(forecastBox);
+        }
+      });
+  }
+
+  searchBtn.addEventListener('click', function(event) {
+    event.preventDefault();
+    var searchCity = document.querySelector('.search-bar').value;
+    document.querySelector('.search-bar').value = '';
+    getWeatherData(searchCity);
+    getFiveDayForecast(searchCity);
+    addToRecentlySearched(searchCity);
   });
-}
-//end of local storage
-// geo code api
-// http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
-
-// 5 day forecase api
-// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
-
-// Initial API call
-function getWeatherData(city) {
-  var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&APPID='+apiKey+'&units=imperial'
-
-fetch (requestUrl)
-.then(function (response) {
-  return response.json();
-})
-
-.then(function (data) {
-  console.log ('fetch response \n------');
-  console.log(data);
-  cityName.textContent = `${data.name} - ${formattedDate}`;
-  temp.textContent = `Temp: ${data.main.temp} °F`;
-  wind.textContent = `Wind: ${data.wind.speed} MPH`;
-  humidity.textContent = `Humidity: ${data.main.humidity} %`;
-});
-
-
-
-}
-
-// make 5 day forecast function - use a loop
-// 5 day forecast function 
-
-function getFiveDayForecast(city) {
-  var forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&APPID=' + apiKey + '&units=imperial';
-
-  fetch(forecastUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log('5-day forecast response \n------');
-      console.log(data);
-
-      // Clear the existing forecast boxes
-      var forecastDiv = document.querySelector('.forecast-div');
-      forecastDiv.innerHTML = '';
-
-      // Loop through the forecast data and populate the forecast-box divs
-     // Loop through the forecast data for the next 5 days
-for (var i = 0; i < data.list.length; i+=8) {
-  var forecastData = data.list[i];
-  var date = new Date(forecastData.dt * 1000); // Convert the timestamp to a Date object
-  var formattedDate = date.toDateString();
-
-  // Create a new div element for the forecast box
-  var forecastBox = document.createElement('div');
-  forecastBox.className = 'forecast-box';
-  
-  // Set the text content to display the formatted date, temperature, wind speed, and humidity
-  forecastBox.textContent = `${formattedDate} Temp: ${forecastData.main.temp} °F, Wind: ${forecastData.wind.speed} MPH, Humidity: ${forecastData.main.humidity} %`;
-
-  // Append the forecastBox to the .forecast-div
-  forecastDiv.appendChild(forecastBox);
-}
-
-    });
-}
-
-
-
-
-
-
-
-searchBtn.addEventListener('click', function(event) { 
-  event.preventDefault();
-var searchCity = document.querySelector('.search-bar').value;
-document.querySelector('.search-bar').value='';
-getWeatherData(searchCity);
-getFiveDayForecast(searchCity);
-// get 5 day search city
-addToRecentlySearched(searchCity);
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 });
+
 
 
 
